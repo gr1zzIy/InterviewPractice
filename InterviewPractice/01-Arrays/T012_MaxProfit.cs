d7ca9a34c34e3578f1320d8944751c03a7c1dc53
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-06-27 22:59:39 +0300

    Refactor MaxProfit functionality to improve efficiency and add input validation

--- InterviewPractice/01-Arrays/T012_MaxProfit.cs
+++ InterviewPractice/01-Arrays/T012_MaxProfit.cs
@@ -6,12 +6,41 @@ namespace InterviewPractice._01_Arrays;
 public class T012_MaxProfit
 {
     private const string TASK_NUMBER = "012";
+    
+    public static void Run(int[] prices)
+    {
+        int[] array = (int[])prices.Clone();
+        int length = array.Length;
+        
+        if (length < 2)
+            return;
 
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        for (int i = 0; i < length; i++) 
+        {
+            if (array[i] < minPrice) 
+            {
+                minPrice = array[i];
+            }
+            else if (array[i] - minPrice > maxProfit) 
+            {
+                maxProfit = array[i] - minPrice;
+            }
+        }
+
+        Console.WriteLine($"{TASK_NUMBER} - The max profit is: {maxProfit}");
+    }
+/* По рантайму полетіло
     public static void Run(int[] prices)
     {
         int[] array = (int[])prices.Clone();
         int length = array.Length;
         int maxProfit = 0;
+
+        if (length < 2)
+            return;
 
         for (int i = 0; i < length; i++)
         {
@@ -26,4 +55,5 @@ public class T012_MaxProfit
         
         Console.WriteLine($"{TASK_NUMBER} - The max profit is: {maxProfit}");
     }
+*/
 }
