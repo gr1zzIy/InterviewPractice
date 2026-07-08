911cf307e6ce4c88df7387bc6aa87305e0d2b3ed
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-07-08 16:15:18 +0300

    Add DifferenceBetweenArrays functionality and update solved problems count in README

---
+++ InterviewPractice/05-HashSet/T057_DifferenceBetweenArrays.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPractice._05_HashSet;
+
+/// <summary>
+/// T057_DifferenceBetweenArrays.cs ⭐⭐
+/// Умова
+/// Повернути елементи першого списку, яких немає у другому.
+/// </summary>
+public class T057_DifferenceBetweenArrays
+{
+	private const string TASK_NUMBER = "057";
+	
+	public static void Run(int[] first, int[] second)
+	{
+		HashSet<int> diff = new HashSet<int>(first);
+		string originalFirst = string.Join(", ", first);
+		string originalSecond = string.Join(", ", second);
+		
+		foreach (var num in second)
+		{
+			if (diff.Contains(num))
+			{
+				diff.Remove(num);
+			}
+		}
+		
+		string result = string.Join(", ", diff);
+		
+		Console.WriteLine($"{TASK_NUMBER}   - First:  [{originalFirst}]\n\tSecond: [{originalSecond}]\n\t\t=> Result: [{result}]");
+	}
+}
